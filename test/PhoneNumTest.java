be2d0dfab6b319a1a008a941dc12500784ccb76a
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-06-23 17:52:54 +0200

    added more 11 unit test
    that are input3Numbers, input4Numbers, containsAlphabet, containsAlphabets, containsSpecialCase, containsSpecialCases, startsWithSpecialCase, startsWithCapitalAlphabet, containsAlphabetWithSpecialCase, containsSpecialCaseOnly, containsAlphabetOnly

--- test/PhoneNumTest.java
+++ test/PhoneNumTest.java
@@ -1,48 +1,129 @@
 import com.company.Main;
 import org.junit.jupiter.api.Test;                              // using test
+
 import static org.junit.jupiter.api.Assertions.assertThrows;    //for exception test
+
 import Exception.outOfScope;                                    // call the exception class to check it here
+
 import java.util.Arrays;                                        //when checking with the output
 import java.util.List;
+
 import static org.testng.AssertJUnit.assertEquals;
 
 public class PhoneNumTest {
 
-    @Test public void test1Number() throws outOfScope {
-        Main myUnit = new Main();
-
-        List<String> result = myUnit.letterCombinations("2");
-        List<String> expectedResult = Arrays.asList("a", "b", "c");
-        assertEquals(expectedResult, result);
+    @Test
+    public void input1Number() throws outOfScope {
+        List<String> result = Main.letterCombinations("2");
+        assertEquals(Arrays.asList("a", "b", "c"), result);
     }
 
-    @Test public void test2Numbers() throws outOfScope {
-        Main myUnit = new Main();
-
-        List<String> result = myUnit.letterCombinations("23");
-        List<String> expectedResult = Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf");
-        assertEquals(expectedResult, result);
+    @Test
+    public void input2Numbers() throws outOfScope {
+        List<String> result = Main.letterCombinations("23");
+        assertEquals(Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"), result);
     }
 
-    @Test public void removeThe1s() throws outOfScope {
-        Main myUnit = new Main();
+    @Test
+    public void input3Numbers() throws outOfScope {
+        List<String> result = Main.letterCombinations("354");
+        assertEquals(Arrays.asList("djg", "djh", "dji", "dkg", "dkh", "dki", "dlg", "dlh", "dli", "ejg", "ejh", "eji", "ekg", "ekh"
+                , "eki", "elg", "elh", "eli", "fjg", "fjh", "fji", "fkg", "fkh", "fki", "flg", "flh", "fli"), result);
+    }
 
-        List<String> result = myUnit.letterCombinations("511");
-        List<String> expectedResult = Arrays.asList("j", "k", "l");
-        assertEquals(expectedResult, result);
+    @Test
+    public void input4Numbers() throws outOfScope {
+        List<String> result = Main.letterCombinations("9276");
+        assertEquals(Arrays.asList("wapm", "wapn", "wapo", "waqm", "waqn", "waqo", "warm", "warn", "waro", "wasm", "wasn", "waso", "wbpm", "wbpn", "wbpo", "wbqm", "wbqn", "wbqo", "wbrm", "wbrn", "wbro", "wbsm", "wbsn", "wbso", "wcpm", "wcpn", "wcpo", "wcqm", "wcqn", "wcqo", "wcrm", "wcrn", "wcro", "wcsm", "wcsn", "wcso",
+                "xapm", "xapn", "xapo", "xaqm", "xaqn", "xaqo", "xarm", "xarn", "xaro", "xasm", "xasn", "xaso", "xbpm", "xbpn", "xbpo", "xbqm", "xbqn", "xbqo", "xbrm", "xbrn", "xbro", "xbsm", "xbsn", "xbso", "xcpm", "xcpn", "xcpo", "xcqm", "xcqn", "xcqo", "xcrm", "xcrn", "xcro", "xcsm", "xcsn", "xcso",
+                "yapm", "yapn", "yapo", "yaqm", "yaqn", "yaqo", "yarm", "yarn", "yaro", "yasm", "yasn", "yaso", "ybpm", "ybpn", "ybpo", "ybqm", "ybqn", "ybqo", "ybrm", "ybrn", "ybro", "ybsm", "ybsn", "ybso", "ycpm", "ycpn", "ycpo", "ycqm", "ycqn", "ycqo", "ycrm", "ycrn", "ycro", "ycsm", "ycsn", "ycso",
+                "zapm", "zapn", "zapo", "zaqm", "zaqn", "zaqo", "zarm", "zarn", "zaro", "zasm", "zasn", "zaso", "zbpm", "zbpn", "zbpo", "zbqm", "zbqn", "zbqo", "zbrm", "zbrn", "zbro", "zbsm", "zbsn", "zbso", "zcpm", "zcpn", "zcpo", "zcqm", "zcqn", "zcqo", "zcrm", "zcrn", "zcro", "zcsm", "zcsn", "zcso"), result);
+    }
+
+    @Test
+    public void removeThe1s() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("511");
+        assertEquals(Arrays.asList("j", "k", "l"), result);
+    }
+
+    @Test
+    public void containsAlphabet() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("3a");
+        assertEquals(Arrays.asList("d", "e", "f"), result);
+    }
+
+    @Test //more than one alphabet
+    public void containsAlphabets() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("a3a");
+        assertEquals(Arrays.asList("d", "e", "f"), result);
+    }
+
+    @Test
+    public void containsSpecialCase() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("93$");
+        assertEquals(Arrays.asList("wd", "we", "wf", "xd", "xe", "xf", "yd", "ye", "yf", "zd", "ze", "zf"), result);
+    }
+
+    @Test //more than one special case
+    public void containsSpecialCases() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("@93$");
+        assertEquals(Arrays.asList("wd", "we", "wf", "xd", "xe", "xf", "yd", "ye", "yf", "zd", "ze", "zf"), result);
+    }
+
+    @Test
+    public void startsWithSpecialCase() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("$28");
+        assertEquals(Arrays.asList("at", "au", "av", "bt", "bu", "bv", "ct", "cu", "cv"), result);
+    }
+
+    @Test
+    public void startsWithCapitalAlphabet() throws outOfScope {
+
+        List<String> result = Main.letterCombinations("A36");
+        assertEquals(Arrays.asList("dm", "dn", "do", "em", "en", "eo", "fm", "fn", "fo"), result);
+    }
+
+    @Test
+    public void containsAlphabetWithSpecialCase() throws outOfScope {
+        List<String> result = Main.letterCombinations("6a$");
+        assertEquals(Arrays.asList("m", "n", "o"), result);
     }
 
 
-    //when exceeding the limit of numbers
-    @Test public void longInput() {
-        Main myUnit = new Main();
-        Throwable exception = assertThrows(outOfScope.class, () -> {myUnit.letterCombinations("326666");});
+
+    @Test
+    public void containsSpecialCaseOnly() {
+        Throwable exception = assertThrows(outOfScope.class, () -> {
+            Main.letterCombinations("$%^&");
+        });
     }
 
-    //empty input
-    @Test public void nullInput() {
-        Main myUnit = new Main();
-        Throwable exception = assertThrows(outOfScope.class, () -> {myUnit.letterCombinations("");});
+    @Test
+    public void containsAlphabetOnly() {
+        Throwable exception = assertThrows(outOfScope.class, () -> {
+            Main.letterCombinations("aaa");
+        });
+    }
+
+    @Test
+    public void longInput() {
+        Throwable exception = assertThrows(outOfScope.class, () -> {
+            Main.letterCombinations("326666");
+        });
+    }
+
+    @Test
+    public void nullInput() {
+
+        Throwable exception = assertThrows(outOfScope.class, () -> {
+            Main.letterCombinations("");
+        });
     }
 
 }
